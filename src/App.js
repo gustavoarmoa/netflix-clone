f8136e0984fe0b9cee9c6b5833496fa169ce0821
gustavoarmoa <gustavo.armoa@outlook.com>
2021-11-26 16:50:33 -0300

    Configuração inicial de arquivos

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Netflix <code>src/App.js</code> clone App.
         </p>
         <a
           className="App-link"
@@ -15,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Projeto inicial
         </a>
       </header>
     </div>
